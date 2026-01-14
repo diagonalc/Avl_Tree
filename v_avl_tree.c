fa281cafab5386d2985b0c6d121a19a33406c0d5
vincenthau07 <hwsv2007@gmail.com>
2026-01-14 15:37:01 +0800

    hehehe

---
+++ v_avl_tree.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct dummy{
+    int val;
+    int height;
+    struct dummy* left;
+    struct dummy* right;
+} node;
+
+bool search(node *node_ptr, int val){
+    if(node_ptr == NULL)
+        return 0;
+    if(val == node_ptr->val)
+        return 1;
+    if(val < node_ptr->val)
+        return search(node_ptr->left, val);
+    else
+        return search(node_ptr->right, val);
+}
+
+node* insert(node *node_ptr, int val){
+    if(node_ptr)
+}
