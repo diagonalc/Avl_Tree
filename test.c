84bdb263f7b5a155a6b377f57aaa1bd53d79dc1b
Diagonalc <diagonalchen@outlook.com>
2026-07-12 23:39:39 +0800

    docker

--- test.c
+++ test.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-struct edge
+int main(int argc, char **argv)
 {
-    int x;
-    int y;
-};
-
-int main()
-{
-    struct edge e[10] = {0,0};
-   
-    printf("%d %d", e[2].x, e[2].y);
+    int a = 0;
+    int b;
+    if ((b = a + 1) > 3)
+    {
+        printf("impossible");
+    }
+    printf("%d", b);
 }
