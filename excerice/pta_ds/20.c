5d1175717304b269aeffe219135adafdba089bdb
Diagonalc <diagonalchen@outlook.com>
2026-07-13 23:06:59 +0800

    unix io

--- excerice/pta_ds/20.c
+++ excerice/pta_ds/20.c
@@ -4,18 +4,24 @@
 
 int graph[MAX_V][MAX_V];
 
-void init_graph(){
-    for (int i = 0; i < MAX_V;i++){
-        for (int j = 0; j < MAX_V;j++){
+void init_graph()
+{
+    for (int i = 0; i < MAX_V; i++)
+    {
+        for (int j = 0; j < MAX_V; j++)
+        {
             graph[i][j] = -1;
         }
     }
 }
 
-void insert(int v1, int v2, int weight){
+void insert(int v1, int v2, int weight)
+{
     graph[v1][v2] = graph[v2][v1] = weight;
 }
 
-int main(){
-    
+// unix io
+int main()
+{
+    return 0;
 }
