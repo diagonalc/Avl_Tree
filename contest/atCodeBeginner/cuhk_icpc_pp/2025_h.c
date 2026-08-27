18fe09560d10545fe936afcd3aaf07bfe9bb20c4
Diagonalc <diagonalchen@outlook.com>
2026-08-27 16:18:08 +0800

    sve

---
+++ contest/atCodeBeginner/cuhk_icpc_pp/2025_h.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[1000];
+    int k;
+    int dc = 0;
+    scanf("%s", s);
+    int l = strlen(s);
+    scanf("%d", &k);
+    if (k > l)
+    {
+        printf("impossible");
+    }
+    int abc[27] = {0};
+    for (int i = 0; i < l; i++)
+    {
+        abc[s[i] - 'a'] = 1;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (abc[i] == 1)
+        {
+            dc++;
+        }
+    }
+    int same = l - dc;
+}
